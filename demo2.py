0b0d353413c42cdb983b402b25f9b2b8a24cbaf8
your name <2876946748@qq.com>
2022-07-20 15:31:41 +0800

    类提交

--- demo2.py
+++ demo2.py
@@ -1,11 +1,18 @@
 class Person():
     name = 'ayi'
     age = 18
+    sex = '男'
 
     def eat(self,food):
         print('吃{}'.format(food))
         return '吃完了'
+    
+    def __init__(self,sex,highth):
+        self.sex = sex
+        self.highth = highth
 
 
-p = Person()
+p = Person('女',177)
 print(p.eat('辣条'))
+print(p.sex)
+print(p.highth)
